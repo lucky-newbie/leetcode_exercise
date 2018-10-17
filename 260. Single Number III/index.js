7dda459d9c31931840d556b405213fa75d3c5880
823844596 <823844596@qq.com>
2018-10-17 09:20:15 +0800

    add leetcode 260

---
+++ 260. Single Number III/index.js
@@ -0,0 +1,36 @@
+/**
+ * 260. Single Number III
+ * https://leetcode.com/problems/single-number-iii/description/
+ * 
+ * 介绍：
+ * Given an array of numbers nums, in which exactly two elements appear only once and
+ * all the other elements appear exactly twice. Find the two elements that appear only once.
+ * 
+ * example:
+ * Input:  [1,2,1,3,2,5]
+ * Output: [3,5]
+ */
+
+ /**
+ * @param {number[]} nums
+ * @return {number[]}
+ * 输出顺序无关
+ */
+var singleNumber = function(nums) {
+  const map = new Map(); // 通过map记录数组中各元素出现次数
+  for (let i = 0; i < nums.length; i++) {
+      if (map.get(nums[i])) {
+          map.set(nums[i], map.get(nums[i]) + 1);
+      } else {
+          map.set(nums[i], 1);
+      }
+  }
+  // 使用map.forEach方法， m（（value， key，mapObj）=> {})
+  const result = [];
+  map.forEach((value, key) => {
+      if (value === 1) {
+          result.push(key)
+      }
+  });
+  return result;
+};
